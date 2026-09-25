7255e61790570c3d87ac004bbe44f303a607ec2c
kkuszyk <kamilkuszyk@hotmail.com>
2026-09-25 17:04:52 +0200

    add basic product information

---
+++ main.py
@@ -0,0 +1,11 @@
+product_id = 1001
+product_name = 'Laptop'
+category = 'Electronics'
+quantity = 15
+price = 799.99
+
+print(product_id)
+print(product_name)
+print(category)
+print(quantity)
+print(price)
