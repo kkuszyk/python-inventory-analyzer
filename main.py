c3e52673e548730b4c1d8d69203c22fd38125b1b
kkuszyk <kamilkuszyk@hotmail.com>
2026-09-25 17:17:42 +0200

    adding calculation of whole stock value

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ category = 'Electronics'
 quantity = 15
 price = 799.99
 
-print(product_id)
-print(product_name)
-print(category)
-print(quantity)
-print(price)
+print(f"Product ID: {product_id}")
+print("Product name: " + product_name)
+print("Category: " + category)
+print(f"Quantity: {quantity}")
+print(f"Price: {price}")
+
+stock_value = quantity * price
+
+print(f"Stock value: {stock_value}")
